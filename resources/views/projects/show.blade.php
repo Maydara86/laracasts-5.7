63f136ca5f436054dfcc4e5f420f0f0552f50b99
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-02-22 23:25:37 +0100

    episode 16

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,5 +3,13 @@
     <h1 class="text-info">{{$project->title}}</h1>
     <hr>
     <p class="text-secondary lead">{{$project->description}}</p>
-<a href="/projects/{{$project->id}}/edit" class="btn btn-primary">Edit</a>
+    @if ($project->tasks->count())
+        @foreach($project->tasks as $task)
+            <ul>
+                <li>{{ $task->description }}</li>
+            </ul>    
+        @endforeach
+    @endif
+    <a href="/projects/{{$project->id}}/edit" class="btn btn-primary">Edit</a>
+
 @endsection
